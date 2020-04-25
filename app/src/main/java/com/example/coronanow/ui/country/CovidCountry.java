fe5cbfb3210365e2343e756a69ad975b83550731
Ricardo Velásquez Aliaga <ricardova1997@gmail.com>
2020-04-25 18:44:41 -0500

    V1 HomeActivity +Datos globales Listado +Países + Casos

---
+++ app/src/main/java/com/example/coronanow/ui/country/CovidCountry.java
@@ -0,0 +1,24 @@
+package com.example.coronanow.ui.country;
+
+public class CovidCountry {
+    String mCovidCountry, mCases, mTodayCases, mDeaths, mTodayDeaths, mRecovered, mTodayRecovered, mCritical;
+
+    public CovidCountry(String mCovidCountry, String mCases) {
+        this.mCovidCountry = mCovidCountry;
+        this.mCases = mCases;
+        this.mTodayCases = mTodayCases;
+        this.mDeaths = mDeaths;
+        this.mTodayDeaths = mTodayDeaths;
+        this.mRecovered = mRecovered;
+        this.mTodayRecovered = mTodayRecovered;
+        this.mCritical = mCritical;
+    }
+
+    public String getmCovidCountry() {
+        return mCovidCountry;
+    }
+
+    public String getmCases() {
+        return mCases;
+    }
+}
